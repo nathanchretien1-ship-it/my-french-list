46cc2f0eb5fb8e37591f8ecfc4189353fa5bad02
Votre Pseudo <nathan.chretien.1@etudiant.univ-rennes.fr>
2026-02-06 08:55:43 +0100

    correction middlexare to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // 1. Création d'une réponse initiale qui permettra de modifier les cookies
   let response = NextResponse.next({
     request: {
